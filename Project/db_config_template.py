3afb29a96c8d2c45b4fecc17451af6721558e98e
joshjbm <joshjb87@gmail.com>
2025-05-06 15:21:48 -0400

    Minor tweaks

---
+++ Project/db_config_template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+This is a blank template for the database configuration file.
+Please fill in the required fields with your database credentials.
+"""
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': '<username>',
+    'password': '<pwd>',
+    'database': '<database>'
+}
